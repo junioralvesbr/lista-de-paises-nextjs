a3dce467ef37515fa591b1a6c052c5e41fa93d64
Junior Alves <jrnalves@gmail.com>
2023-12-11 01:30:59 -0300

    create Card components and getAllCountries function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,22 @@
-export default function Home() {
+import { Card } from "@/components/Card"
+
+async function getAllCountries() {
+  const response = await fetch('https://restcountries.com/v3.1/all')
+
+  if (!response.ok) {
+    throw new Error('failed to fetch data')
+  }
+
+  const data = await response.json()
+  return data
+}
+
+export default async function Home() {
+  const countries = await getAllCountries()
+
   return (
-    <h1>Hellow World</h1>
+    <section>
+      <Card countries={countries} />
+    </section>
   )
 }
