4532e9959772b4732f81aedddc781a0e1804a136
Junior Alves <jrnalves@gmail.com>
2023-12-12 17:28:46 -0300

    create search box

--- src/components/TopBar/index.tsx
+++ src/components/TopBar/index.tsx
@@ -1,18 +1,23 @@
 import Image from "next/image"
+import { SearchBox } from "../SearchBox"
 
 export function TopBar() {
   return (
-    <div className="flex items-center gap-2 py-7">
-      <Image
-        src="/logo.svg"
-        alt="Logo do site"
-        width={50}
-        height={50}
-      />
+    <div className="flex items-center justify-between">
+      <div className="flex items-center gap-2 py-7">
+        <Image
+          src="/logo.svg"
+          alt="Logo do site"
+          width={50}
+          height={50}
+        />
 
-      <h4 className="text-2xl font-bold">
-        Países do mundo
-      </h4>
+        <h4 className="text-2xl font-bold">
+          Países do mundo
+        </h4>
+      </div>
+
+      <SearchBox />
     </div>
   )
 }
